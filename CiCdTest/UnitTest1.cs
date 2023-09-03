aa4d9821cfa46186dfbb678b69e7b526b3e1f09a
m.zolfaghari <mzolfaghari71@yahoo.com>
2023-09-03 17:11:10 +0330

    this is test for ci/cd

--- CiCdTest/UnitTest1.cs
+++ CiCdTest/UnitTest1.cs
@@ -8,7 +8,14 @@ namespace CiCdTest
         [TestMethod,Fact]
         public void TestMethod1()
         {
+            // Arrange
+            var myvar = "test";
 
+            //Act
+            bool result = (myvar == "test");
+
+            //Assert 
+            Xunit.Assert.True(result);
         }
         [Fact]
         public void Just_For_Run_Sample_Test()
